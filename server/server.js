4f700e563f515abc10bcb273df40c92cce5deb91
Your Name <totakhil.bahram@student.greenriver.edu>
2025-10-02 11:33:00 -0700

    server.js and succefully ran hello world app

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 
+const express = require("express");
+const app = express();
 
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
